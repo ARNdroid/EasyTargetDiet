f3c8ac9f9676cb5efe16b5b861db0a274d14d53e
alceurneto <alceurneto@gmail.com>
2013-10-16 09:50:03 -0300

    Week Period created and tested.

--- EasyTargetDiet/src/main/java/br/com/arndroid/etdiet/virtualweek/WeekPeriod.java
+++ EasyTargetDiet/src/main/java/br/com/arndroid/etdiet/virtualweek/WeekPeriod.java
@@ -1,24 +1,77 @@
 package br.com.arndroid.etdiet.virtualweek;
 
+import android.content.Context;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import br.com.arndroid.etdiet.provider.parametershistory.ParametersHistoryManager;
+
 public class WeekPeriod {
 
-    private long initialDate;
-    private long finalDate;
-    private long referenceDate;
+    private Date mInitialDate;
+    private Date mFinalDate;
+    private Date mReferenceDate;
 
-    public WeekPeriod(long referenceDate) {
-        // TODO: to implement.
+    public WeekPeriod(Context context, Date referenceDate) {
+
+        mReferenceDate = referenceDate;
+        setInitialAndFinalDates(context, referenceDate);
     }
 
-    public long getInitialDate() {
-        return initialDate;
+    private void setInitialAndFinalDates(Context context, Date referenceDate) {
+
+        int trackingWeekday = new ParametersHistoryManager(context).getTrackingWeekdayForDate(referenceDate);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mReferenceDate);
+        int referenceDateWeekday = calendar.get(Calendar.DAY_OF_WEEK);
+        int deltaDays;
+
+        switch (trackingWeekday) {
+            case Calendar.SUNDAY:
+                deltaDays = Calendar.SATURDAY - referenceDateWeekday;
+                break;
+            case Calendar.MONDAY:
+                deltaDays = Calendar.SUNDAY - referenceDateWeekday;
+                break;
+            case Calendar.TUESDAY:
+                deltaDays = Calendar.MONDAY - referenceDateWeekday;
+                break;
+            case Calendar.WEDNESDAY:
+                deltaDays = Calendar.TUESDAY - referenceDateWeekday;
+                break;
+            case Calendar.THURSDAY:
+                deltaDays = Calendar.WEDNESDAY - referenceDateWeekday;
+                break;
+            case Calendar.FRIDAY:
+                deltaDays = Calendar.THURSDAY - referenceDateWeekday;
+                break;
+            case Calendar.SATURDAY:
+                deltaDays = Calendar.FRIDAY - referenceDateWeekday;
+                break;
+            default:
+                throw new IllegalStateException("Invalid trackingWeekday = " + trackingWeekday);
+        }
+
+        if(deltaDays < 0) {
+            deltaDays += 7;
+        }
+
+        calendar.add(Calendar.DAY_OF_MONTH, deltaDays);
+        mFinalDate = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_MONTH, -6);
+        mInitialDate = calendar.getTime();
     }
 
-    public long getFinalDate() {
-        return finalDate;
+    public Date getInitialDate() {
+        return mInitialDate;
     }
 
-    public long getReferenceDate() {
-        return referenceDate;
+    public Date getFinalDate() {
+        return mFinalDate;
+    }
+
+    public Date getReferenceDate() {
+        return mReferenceDate;
     }
 }
